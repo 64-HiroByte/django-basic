ed21d2a1e30dd28d1a38021cf175a95329e79d33
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-02 21:40:52 +0900

    TodoDeleteクラスを定義し、delete.htmlファイルを新規作成した

--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView,DeleteView
 from .models import TodoModel
 
 
@@ -20,3 +20,9 @@ class TodoCreate(CreateView):
     model = TodoModel
     fields = ('title', 'memo', 'priority', 'duedate')
     success_url =  reverse_lazy('list')
+
+
+class TodoDelete(DeleteView):
+    template_name = "delete.html"
+    model = TodoModel
+    success_url = reverse_lazy('list')
