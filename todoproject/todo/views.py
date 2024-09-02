2994c71512fa3665858b3d02e72a485bc2f69472
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-02 21:55:21 +0900

    TodoUpdateクラスを定義し、update.htmlファイルを新規作成した

--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView,DeleteView
+from django.views.generic import ListView, DetailView, CreateView,DeleteView, UpdateView
 from .models import TodoModel
 
 
@@ -26,3 +26,11 @@ class TodoDelete(DeleteView):
     template_name = "delete.html"
     model = TodoModel
     success_url = reverse_lazy('list')
+
+
+class TodoUpdate(UpdateView):
+    template_name = "update.html"
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')
+    
