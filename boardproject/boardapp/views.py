53f1efac9b4e6516a3224ce05fdceb287e42c00d
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-03 22:57:00 +0900

    views.pyでユーザー登録する処理を実装した

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -4,15 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def signupfunc(request):
-    object_list = User.objects.all()
-    object = User.objects.get(username='hiro')
-    print(object_list)
-    print(object)
-    print(object.email)
     
-    if request.method=='POST':
-        print('this is post method')
-    else:
-        print('this is not post method')
+    if request.method == 'POST':
+        # print(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
     
     return render(request, 'signup.html', {'some':100})
