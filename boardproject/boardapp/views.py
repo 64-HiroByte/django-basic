31f71bb18219305e08dc062876cbbbdf7eeb6d57
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-03 21:43:14 +0900

    HTTP requestのmethodが'POST'の場合の処理を追加

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def signupfunc(request):
-    return render(request, 'signup.html', {})
+    if request.method=='POST':
+        print('this is post method')
+    else:
+        print('this is not post method')
+    
+    return render(request, 'signup.html', {'some':100})
