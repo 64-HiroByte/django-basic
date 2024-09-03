52ecd035b2643074d0f064b2950ce4041492f97c
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-03 23:09:26 +0900

    ユーザー登録時の重複データに対してIntegrityErrorをキャッチできるようにした

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -9,6 +10,11 @@ def signupfunc(request):
         # print(request.POST)
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some':100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています。'})
+            
     
     return render(request, 'signup.html', {'some':100})
