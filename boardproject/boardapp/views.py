8c0adf55df6b2fc4cbcdc8f5b79e0f815c1d0915
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-03 22:00:31 +0900

    Userモデルをインポートして、migrateコマンドを実行した

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def signupfunc(request):
+    object_list = User.objects.all()
+    object = User.objects.get(username='hiro')
+    print(object_list)
+    print(object)
+    print(object.email)
+    
     if request.method=='POST':
         print('this is post method')
     else:
