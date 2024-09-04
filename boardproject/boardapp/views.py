22fea1c4b24f5266eda5fc1a4e604cb760e223d4
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-04 22:02:30 +0900

    login機能の実装をした

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 
@@ -18,3 +19,17 @@ def signupfunc(request):
             
     
     return render(request, 'signup.html', {'some':100})
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return render(request, 'login.html', {'context':'logged in'})
+        else:
+            return render(request, 'login.html', {'context':'not logged in'})
+    
+    return render(request, 'login.html', {'context':'get method'})
