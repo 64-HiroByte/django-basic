872a52ad4b98325c9b4181dc20891cd6c83f0a70
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-05 18:46:27 +0900

    list.htmlに投稿内容のリストを表示するようにした（表示させるデータも登録した）

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 
+from .models import BoardModel
+
 
 # Create your views here.
 def signupfunc(request):
@@ -36,4 +38,5 @@ def loginfunc(request):
 
 
 def listfunc(request):
-    return render(request, 'list.html', {})
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list':object_list})
