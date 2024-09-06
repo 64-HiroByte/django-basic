746e84993b15674edbe4cc56a8aec6e3d3cdad78
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-06 22:56:41 +0900

    logout機能を実装した

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db import IntegrityError
@@ -42,3 +42,8 @@ def loginfunc(request):
 def listfunc(request):
     object_list = BoardModel.objects.all()
     return render(request, 'list.html', {'object_list':object_list})
+
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
