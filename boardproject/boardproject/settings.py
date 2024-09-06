ddfaeb16f41434069c31af977afb0b4a88f3b3d5
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-06 19:22:29 +0900

    cssファイルを扱うための設定をし、style.cssファイルを新規作成した

--- boardproject/boardproject/settings.py
+++ boardproject/boardproject/settings.py
@@ -118,13 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/sta/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # この設定はデプロイ時（本番環境）に必要になる
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = 'medi/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = BASE_DIR / 'media'
-
-MEDIA_URL = 'medi/'
