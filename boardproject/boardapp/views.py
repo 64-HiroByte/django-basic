9545dd4c11041a998ecacaf289cd9baac9ed4d90
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-07 17:33:47 +0900

    createviewクラスの新規作成と新規投稿画面を実装

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 from .models import BoardModel
 
@@ -71,3 +73,10 @@ def readfunc(request, pk):
         object.readtext = object.readtext + ' ' + username
         object.save()
         return redirect('list')
+
+
+class BoardCreate(CreateView):
+    template_name = 'create.html'
+    model = BoardModel
+    fields = ('title', 'content', 'author', 'snsimage')
+    success_url = reverse_lazy('list')
